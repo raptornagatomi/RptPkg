82528380eb0c726a71dfb2787804ce692404205d
Raptor <raptor.evo@gmail.com>
2018-05-11 21:34:07 +0800

    Add the initial revision of RptPkg
    RptPkg (Raptor's Package): Will used for the collection of my UEFI utilities.
    
    * A Hello World sample was also added.

---
+++ Application/Hello/Hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/***
+        Print a welcoming message.
+
+        Establishes the main structure of the application.
+
+        @retval  0         The application exited normally.
+        @retval  Other     An error occurred.
+***/
+int main(int argc, char* argv[])
+{
+        printf("Hello there fellow Programmer.\n");
+
+        return(0);
+}
